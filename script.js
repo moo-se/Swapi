22ab396ea6682c5f683ef295c4b9c90088a54c83
zuwa <omozuwa.omede@decagon.dev>
2023-06-14 09:14:21 +0100

    added detailed comments to script.js file

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-import { char } from './chars.js';
+import { char } from './chars.js'; //import character images from char.js file
 
 let page = 'https://swapi.dev/api/people'
 
 /*async*/ function fetchData(url){
 
-    fetch(url) //'https://swapi.dev/api/people', 'https://swapi.dev/api/people/?page=2'
+    fetch(url) //fetch data from url
     .then(res =>
          {return res.json()}) //wait to receive data and then parse it
    
-    .then(data => { //element is 0,1,2,... array index
+    .then(data => { // perform logic on data
         data.results.forEach((element,index) => {
-            //display func
-            let list = document.getElementById('myList')
-            let li = document.createElement('li')
-            li.innerText = element.name
-            list.appendChild(li)
-            li.addEventListener("click", function(){
-                    let name = document.querySelectorAll('[id="name"]')//document.getElementById('name')
-                    let gender = document.getElementById('gender')
-                    let height = document.getElementById('height')
-                    let image = document.getElementById('image')
+            //element is 0,1,2... i.e array[index]
+            let list = document.getElementById('myList') //get parent element <ul>
+            let li = document.createElement('li') // create child element <li>
+            li.innerText = element.name // set li innertext
+            list.appendChild(li) //append <li> element to parent element <ul>
+            li.addEventListener("click", function(){      //add an event to <li> element
+                    let name = document.querySelectorAll('[id="name"]')//get all elements with name id and store it in name variable
+                    let gender = document.getElementById('gender') //get element with gender id and store it in gender variable
+                    let height = document.getElementById('height') //get element with height id and store it in height variable
+                    let image = document.getElementById('image') //get element with image id and store it in an image variable
 
                     name.forEach(i => {
-                        i.innerText = element.name.toUpperCase();
+                        i.innerText = element.name.toUpperCase(); //for each element with name id, let its innertext equals the name of character
                         
                     });
                    
-                    gender.innerText = `Gender: ${element.gender.toUpperCase()}`; //element.gender
-                    height.innerText = `Height: ${element.height.toUpperCase()}`;//element.height
-                    image.src = char[index]
+                    gender.innerText = `Gender: ${element.gender.toUpperCase()}`; 
+                    height.innerText = `Height: ${element.height.toUpperCase()}`;
+                    image.src = char[index] //maps data.results index to char array in chars.js file
                     
-                },false) //event: on click display img,name,height, and gender of selected item
+                },false) //event: on click display img,name,height, and gender of selected character
 
             
         }); // end of foreach block
 
        if ( data.next ){
             fetchData(data.next)
-        } //if there are next pages, display data
+        } //if there are next pages, invoke funtion again and again
         
         
     }) // end of then block
